37ae5503ba8d13a438fa193a129f2588144cc29f
Diego Ademir <dadhemir@gmail.com>
2024-11-27 20:56:32 -0500

    update variables de entorno

--- scan-log-AI.py
+++ scan-log-AI.py
@@ -1,11 +1,14 @@
+import os
 import openai
 import pandas as pd
+from dotenv import load_dotenv
 
-# Set up OpenAI API key (make sure to replace 'your-api-key' with your actual API key)
-openai.api_key = '### your Key ###'
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+openai.api_key = OPENAI_API_KEY
 
 # Load the firewall log file. View input folder
-file_path = 'log-fw.xlsx'
+file_path = 'input/log-fw.xlsx'
 df = pd.read_excel(file_path)
 
 # Preprocess the log data (adjust this based on the actual structure of your log file)
